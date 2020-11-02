d1fa320382ae38c69e00f30467aab3f54959ab8e
leeton1412 <leighton.griffith87@gmail.com>
2020-11-02 15:59:07 +0000

    Countdown implemented

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+document.addEventListener('DOMContentLoaded', () => {
+    const timeLeftDisplay = document.querySelector('#time-left')
+    const startBtn = document.querySelector('#start-button')
+    let timeLeft = 10
+
+    function countDown (){
+        setInterval(function(){
+            if(timeLeft <= 0){
+                clearInterval(timeLeft = 0)
+            }
+            timeLeftDisplay.innerHTML = timeLeft
+            timeLeft -= 1
+        }, 1000)
+    }
+
+    startBtn.addEventListener('click', countDown)
+
+} )
