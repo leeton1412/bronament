c76348dec8bf308dd36ec8841aa5d74b679a8410
leeton1412 <leighton.griffith87@gmail.com>
2020-11-03 23:55:52 +0000

    Added Audio for each number & Countdown auto load

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     const timeLeftDisplay = document.querySelector('#time-left')
     const startBtn = document.querySelector('#start-button')
     let timeLeft = 10
 
     function countDown (){
+        const nine = new Audio('assets/sounds/9.mp3')
+        const eight = new Audio('assets/sounds/8.mp3')
+        const seven = new Audio('assets/sounds/7.mp3')
+        const six = new Audio('assets/sounds/6.mp3')
+        const five = new Audio('assets/sounds/5.mp3')
+        const four = new Audio('assets/sounds/4.mp3')
+        const three = new Audio('assets/sounds/3.mp3')
+        const two = new Audio('assets/sounds/2.mp3')
+        const one = new Audio('assets/sounds/1.mp3')
+        
         setInterval(function(){
             if(timeLeft <= 0){
                 clearInterval(timeLeft = 0)
             }
+           switch(timeLeft){
+                case 9:
+                    nine.play();
+                    break;
+                case 8:
+                    eight.play();
+                    break;
+                case 7:
+                    seven.play();
+                    break;
+                case 6: 
+                    six.play();
+                    break;
+                case 5:
+                    five.play();
+                    break;
+                case 4:
+                    four.play();
+                    break;
+                case 3:
+                    three.play()
+                    break;
+                case 2:
+                    two.play();
+                    break;
+                case 1:
+                    one.play();
+                    break;
+            }
+            
+
             timeLeftDisplay.innerHTML = timeLeft
-            new Audio('https://gofile.io/d/rJASwl').play()
             timeLeft -= 1
         }, 1000)
+    
     }
-
+    countDown();
     startBtn.addEventListener('click', countDown)
-
-} )
+});
