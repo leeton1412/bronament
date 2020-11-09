bdda63e6b47512d13af5d2f5572d80dae9a6dc9f
leeton1412 <leighton.griffith87@gmail.com>
2020-11-09 10:43:58 +0000

    onClick created and var tested

--- assets/javascript/tournament.js
+++ assets/javascript/tournament.js
@@ -1,1 +1,7 @@
-console.log("helllooooooooo world")
+//Start with a onclick event to get the value of players
+
+function playerAmount(){
+    var players = prompt("Amount of players? (2, 4 or 8");
+        console.log(players);
+
+}
